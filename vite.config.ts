4d71df9e63999b228b941fa9031379c51f700f88
ngothingocle <ngoclecyt@gmail.com>
2026-08-19 09:25:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/covanhoctap2/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
